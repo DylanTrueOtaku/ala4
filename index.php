28d7aa8b0143ed3d345972d61ec0803d41eac7a2
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-05-24 09:44:11 +0200

    Edited index

--- index.php
+++ index.php
@@ -6,24 +6,3 @@
 
 </body>
 <?php include 'footer.php'?>
-<?php
-
-    require_once('room.class.php');
-    $room = new roomClass();
-
-
-    if ($_GET) {
-        print_r($_GET);
-        $id = $_GET['id'];
-
-        if ($id) {
-            $room->show($id);
-        }
-    } else {
-         $room->index();
-    }
-
-
-
-
-?>
