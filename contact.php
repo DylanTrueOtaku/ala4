a6e79f2d36e48d878f2999a62acee3f0d111f1f6
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-06 08:50:10 +0200

    added some style and edited the menu

--- contact.php
+++ contact.php
@@ -1,17 +1,19 @@
 <?php include 'header.php'?>
 
 <body>
-  <div id="Form">
-    <div>
-      <form>
-        First name:<br>
-        <input type="text" name="firstname"><br>
-        Last name:<br>
-        <input type="text" name="lastname"><br>
-        Email:<br>
-        <input type="text" name="email"><br>
-      </form>
-    </div>
+  <div id="form">
+
+    <form>
+      First name:<br>
+      <input type="text" name="firstname"><br>
+      Last name:<br>
+      <input type="text" name="lastname"><br>
+      Email:<br>
+      <input type="text" name="email"><br>
+    </form>
+  </div>
+  <div id="forminfo">
+    owo
   </div>
 
 </body>
