cec3a644e8dd8f0c3c7e7564e890f6b78e54f12c
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-06 10:47:21 +0200

    finished coding contact.php

--- contact.php
+++ contact.php
@@ -10,10 +10,19 @@
       <input type="text" name="lastname"><br>
       Email:<br>
       <input type="text" name="email"><br>
+      Descripe your problem here:<br>
+      <textarea type="text" name="email" id="problemdes"></textarea><br>
     </form>
+    <button>Submit</button>
   </div>
   <div id="forminfo">
-    owo
+    If we dont respond within an week please contact us.
+    Our email and phone number will be put in this bar,
+    Dont hesitate to call us if your qeuestion is important.
+    <br><br>
+    Email: Hotelcal@hotmail.com
+    <br><br>
+    PN: 0172 01 23 45 67
   </div>
 
 </body>
