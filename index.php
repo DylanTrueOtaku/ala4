f3cd74e03901634675c83550d432d5ca8bbb498f
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-13 09:52:47 +0200

    finished the homepage

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 <?php include 'header.php'?>
 
 <body>
-  <div></div>
-  <div></div>
+  <div id="homepage"></div>
+  <div id="homepageinfo">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sem justo, fermentum ac justo ut, facilisis sollicitudin ex. Nunc rutrum eu est eu iaculis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas eu posuere ex. Donec a nunc in nibh sagittis volutpat in a tortor. Proin dapibus risus sed mi imperdiet volutpat. Pellentesque sagittis sem ac purus pulvinar, consequat interdum dui tristique. Aliquam vehicula velit quis aliquam faucibus.
+    </p>
+    <p>
+      Aenean dignissim sit amet enim at efficitur. Integer nec velit vehicula, sagittis neque sed, rhoncus nisi. Suspendisse condimentum nisi nec velit dignissim blandit. Sed vitae vulputate erat. Vestibulum efficitur, libero ut elementum venenatis, velit nisl viverra est, nec mollis lorem mauris vel quam. Vestibulum quis tincidunt elit. Donec ultricies, ipsum sit amet finibus sagittis, dui quam mollis diam, vitae lacinia nulla risus sit amet nisi. Aliquam gravida fermentum nibh. Sed elit lacus, eleifend a augue ut, laoreet rutrum velit. Nullam laoreet dapibus dui vel fermentum. Quisque tristique rutrum augue, at vehicula magna ornare quis. Morbi egestas, nunc pretium posuere lacinia, sem neque euismod quam, nec rutrum risus arcu sodales justo. Nunc et mi eros.</p>
+  </div>
 </body>
 <?php include 'footer.php'?>
