0e287f8be380327c62eb225ccdf37bdedae7e058
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-20 09:15:42 +0200

    edited the dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 $servername = "127.0.0.1";
 $username = "root";
 $password = "usbw";
-$database = "ala4";
+$database = "hotelcal";
 
 try {
     $conn = new PDO("mysql:host=$servername;port=3306;dbname=$database", $username, $password);
