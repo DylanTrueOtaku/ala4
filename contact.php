6863eebc1ddbb16efab198b12b84e65d2e67bac5
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-20 13:47:43 +0200

    finalized contact page

--- contact.php
+++ contact.php
@@ -18,13 +18,18 @@
   </div>
   <div id="forminfo">
     <p>
-      If we dont respond within an week please contact us.
-      Our email and phone number will be put in this bar,
-      Dont hesitate to call us if your qeuestion is important.
-      <br><br>
-      Email: Hotelcal@hotmail.com
-      <br><br>
-      PN: 0172 01 23 45 67
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga fugit ab, molestiae velit dicta facilis corrupti iure itaque eius, sed illo inventore incidunt cupiditate veritatis harum ea sequi, quo maxime.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo quod, soluta ut accusamus at quasi culpa porro, illo in minus veniam qui. Dignissimos omnis unde, eum culpa consequatur, sunt voluptates?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, sunt! Suscipit quas hic quaerat in repellat, sint libero vel explicabo nulla, praesentium. Placeat eius eveniet, velit dolor laboriosam, eligendi nemo.
+    </p>
+
+    <p>
+      Nam sint ex, voluptatem, beatae obcaecati, necessitatibus assumenda rem tenetur mollitia in vitae illum, quod excepturi odit consequuntur! Cumque praesentium voluptatum id eveniet animi nisi exercitationem eum, autem, voluptas voluptatibus.
     </p>
   </div>
+
 </body> <?php include 'footer.php'?>
