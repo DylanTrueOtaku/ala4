99890586b409f6324d5054ad3da73e67927dab8a
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-21 09:15:12 +0200

    infopage is finished

--- about.php
+++ about.php
@@ -2,8 +2,33 @@
 
 <body>
   <div id='placement'>
-    <div id="map"></div>
-    <div id="mapinfo"></div>
+    <div id="map">
+      <!--Google map-->
+      <div>
+        <iframe src="https://maps.google.com/maps?q=Hotel+California+Todos+Santos&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+
+      <!--Google Maps-->
+    </div>
+    <div id="mapinfo">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum hic placeat, deleniti dolor fuga. Accusamus cum vel atque est quibusdam, reiciendis veritatis similique fugit amet quod ullam totam porro fugiat!
+      </p>
+      <p>
+        Consectetur unde nam ullam impedit dicta quo magni atque explicabo autem laborum similique, obcaecati voluptas assumenda veniam officia natus quos, nisi reiciendis sapiente odio cumque consequatur quis debitis, at quasi!
+      </p>
+      <p>
+        Ducimus repellendus quod commodi quas atque necessitatibus reprehenderit molestiae nihil aspernatur, facere praesentium error fuga dignissimos odio dolore accusamus excepturi natus? Earum illo possimus id obcaecati numquam fugiat, voluptates dolore.
+      </p>
+      <p>
+        Quam sequi, eligendi dolore, animi nesciunt repellat deleniti excepturi quod voluptatum, esse ab odio neque nobis necessitatibus repellendus autem id quisquam explicabo expedita laudantium maxime. Amet voluptates nihil sint similique.
+      </p>
+      <p>
+        Deleniti voluptatum mollitia, doloremque repellat, iste officiis, possimus fuga aspernatur voluptates vero et. Natus quod sequi fugiat ad laborum tempora fugit consequatur, voluptas commodi maiores! Quo, deserunt, fuga! Deleniti, placeat.
+      </p>
+
+
+    </div>
   </div>
 
 </body>
