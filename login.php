3b1973de7a6bf44d217f35e45b7e1e111cc4fff2
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-21 10:22:48 +0200

    added javascript to the login

--- login.php
+++ login.php
@@ -2,7 +2,12 @@
 
 <body>
   <div id='placement'>
+    <script>
+      if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+        header("location: welcome.php");
+        exit;
 
+    </script>
   </div>
 </body>
 <?php include 'footer.php'?>
