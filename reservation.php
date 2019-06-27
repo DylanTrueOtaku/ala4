3e0e45c64c3affd302d949b04a5bed38c84cb94a
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-27 11:20:22 +0200

    edited some things

--- reservation.php
+++ reservation.php
@@ -1,16 +1,15 @@
 <?php include 'header.php'?>
-<link rel="stylesheet" href="calendar.php">
+<link rel="stylesheet" href="calendar.css">
 
 <body>
   <div id='placement'>
-    <?php
+    <div id="calendari">
+      <?php
 include 'calendar.php';
 
 $calendar = new Calendar();
 
 echo $calendar->show();
-?>
+      ?></div>
   </div>
-
-</body>
-<?php include 'footer.php'?>
+</body> <?php include 'footer.php'?>
