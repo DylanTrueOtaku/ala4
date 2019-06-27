9ff5b1df9ebb37ffae799e8929427871ce3a3ecc
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-27 11:21:35 +0200

    deleted the calendar

--- reservation.php
+++ reservation.php
@@ -1,15 +1,8 @@
 <?php include 'header.php'?>
-<link rel="stylesheet" href="calendar.css">
+
 
 <body>
   <div id='placement'>
-    <div id="calendari">
-      <?php
-include 'calendar.php';
 
-$calendar = new Calendar();
-
-echo $calendar->show();
-      ?></div>
   </div>
 </body> <?php include 'footer.php'?>
