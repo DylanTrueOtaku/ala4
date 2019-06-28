b234d9505985419003d5ac4d4059f52e78e0a65f
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-28 13:15:23 +0200

    edited the style
    added some code

--- reservation.php
+++ reservation.php
@@ -3,6 +3,31 @@
 
 <body>
   <div id='placement'>
+    <?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "usbw";
+$database = "hotelcal";
 
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT id, firstname, lastname FROM rooms";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+    ?>
   </div>
 </body> <?php include 'footer.php'?>
