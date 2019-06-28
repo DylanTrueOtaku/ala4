1b75e0696e4e23f4d25da472e0fcf36f776672b6
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-28 14:14:15 +0200

    made the reservation

--- reservation.php
+++ reservation.php
@@ -4,30 +4,48 @@
 <body>
   <div id='placement'>
     <?php
-$servername = "127.0.0.1";
+$servername = "localhost";
 $username = "root";
 $password = "usbw";
 $database = "hotelcal";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+  $link = mysqli_connect($servername, $username, $password, $database);
 
-    $sql = "SELECT id, firstname, lastname FROM rooms";
-    $result = $conn->query($sql);
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+// Attempt select query execution
+$sql = "SELECT * FROM rooms";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>naam</th>";
+                echo "<th>prijs</th>";
+                echo "<th>omschrijving</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr><form>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
+                echo "<td>" . $row['description'] . "<button type='submit'>Bestel</button><form></td>";
+
+            echo "</tr>";
         }
-    } else {
-        echo "0 results";
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
     }
-    $conn->close();
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+// Close connection
+mysqli_close($link);
     ?>
   </div>
 </body> <?php include 'footer.php'?>
